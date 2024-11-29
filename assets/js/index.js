19d5ff1a54953118d75916696bcf2f41a572b69b
Rey Renna <reyrenna4@gmail.com>
2024-11-29 17:48:10 +0100

    stop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 const myHero = document.getElementById("hero");
 let altezza = myHero.offsetHeight;
 console.log(altezza);
+const header = document.getElementById("header");
+
+if (header===altezza){
+  header.classList.replace("header", "header2");
+};
